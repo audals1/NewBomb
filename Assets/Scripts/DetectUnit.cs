fd7096b83a2738832162bd286d51705b5768b9e2
audals1 <audals3@gmail.com>
2024-01-02 00:03:01 +0900

    240101

--- Assets/Scripts/DetectUnit.cs
+++ Assets/Scripts/DetectUnit.cs
@@ -47,7 +47,7 @@ public class DetectUnit : MonoBehaviour
             //폭탄 가진 플레이어 이동 속도 업
 
             otherPlayer.moveSpeed *= 1.5f;
-            currentPlayer.moveSpeed = 5f;
+            currentPlayer.moveSpeed = 5f; 
 
             // 충돌 시 가장 가까운 플레이어를 다시 계산
             Player nearestPlayer = GetNearestPlayer();
