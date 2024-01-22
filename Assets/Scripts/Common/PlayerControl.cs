5a240b9ee3d723aa8c2dbb9956f117c36c23d39c
audals1 <audals3@gmail.com>
2024-01-22 18:28:56 +0900

    240122

--- Assets/Scripts/Common/PlayerControl.cs
+++ Assets/Scripts/Common/PlayerControl.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float jumpForce = 10f;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
 
     private Rigidbody rb;
     private bool isGrounded;
@@ -19,8 +17,8 @@ public class PlayerControl : MonoBehaviour
     private void Update()
     {
         // 땅에 닿아 있는지 체크
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
-
+        isGrounded = Physics.CheckSphere(transform.position, 0.1f);
+        Debug.Log(isGrounded);
         // 점프 입력 처리
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
